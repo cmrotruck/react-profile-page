67fd166e2bde1a542517df26d47d5ade1d2f2eed
cmrotruck <crotruck@gmail.com>
2022-05-18 19:13:58 -0400

    updated bio on bio page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,12 +11,29 @@ const About = () => {
           alt="Portfolio_Photo.JPG"
         />
         <div className="col-6 align-self-center">
-          <blockquote className="col text-center">
+          <blockquote className="col">
             <p className="blockquote">Hello, I am Chad Rotruck.</p>
             <p className="blockquote">
               I am a web developer with 8 years experience in software
-              development. I am also a volunteer coach for Ohio State University
+              development. I recently earned a certificate in Full Stack
+              Development from The Ohio State University focusing on developing
+              skills in JavaScript, CSS, React.js as well as keeping the design
+              responsive.
+            </p>
+            <p className="blockquote">
+              As a computer programmer making the switch to web development, I
+              bring a logical approach to design with a focus on mobile first
+              development. My previous experience with creating applications
+              that communicate with servers and databases have prepared me for
+              current web applications as well as being able to work as a team.
+              In my spare time, I volunteer coach for Ohio State University
               Bowling Club.
+            </p>
+            <p className="blockquote">
+              In my spare time, I volunteer coach for Ohio State University
+              Bowling Club as well as create game controllers for racing
+              simulators, such as steering wheels, shifters, button boxes, and
+              hand breaks.
             </p>
           </blockquote>
         </div>
