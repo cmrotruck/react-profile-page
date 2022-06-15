e72d9a8feae05892daf9b27a9a18aa77c1d5569b
cmrotruck <crotruck@gmail.com>
2022-06-15 18:18:26 -0400

    readded note-taker project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -40,14 +40,15 @@ const Projects = () => {
             deployed="http://bootcamp-zoo.herokuapp.com/"
             skills="HTML, CSS, JavaScript, React.js, Semantic UI for React, MondoDB, Mongoose"
           />
-          {/* <ProjectCard
+          <ProjectCard
             title="Note Taker"
             created="2021"
             description="This is a backend assignment where the front end was already created. We had to create routes to point the html pages and api requests to the correct endpoints an manages a db.json file. This is a task board application. You can create and delete tasks. You can click on notes in the left column to see more info."
             screenshot={require("../../assets/Projects/note-taker-screenshot.png")}
             github="https://github.com/cmrotruck/note_taker"
             deployed="https://note-taker76.herokuapp.com/"
-          /> */}
+            skills="HTML, CSS, JavaScript, React.js, Semantic UI for React, MondoDB, Mongoose"
+          />
           {/* <ProjectCard /> */}
         </Card.Group>
       </Dimmer.Dimmable>
